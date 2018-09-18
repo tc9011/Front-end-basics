b856b00d74ac72e5b7942b5590be7ad18a6664ee
tc9011 <tc9011@126.com>
2018-09-18 20:37:33 +0800

    feat(js): add == and === demo

---
+++ JavaScript/1. == and ===/== and ===.js
@@ -0,0 +1,13 @@
+'5' == 5                      // true
+'5' === 5                     // false
+
+null == undefined             // true
+null === undefined            // false
+
+false == 0                    // true
+false === 0                   // false
+
+{ a: 1 } == { b: 2 }          // false
+
+NaN == NaN                    // false
+NaN != NaN                    // true
