816fb3895a7cb8cc4591a5a6e5bab0a691450865
tc9011 <tc9011@126.com>
2018-09-19 15:14:43 +0800

    feat(js): add var let and const

--- JavaScript/4. var let and const/var let and const.js
+++ JavaScript/4. var let and const/var let and const.js
@@ -33,8 +33,39 @@ for (let i = 0; i < 10; i++) {
 }                                       // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
 
+// no hoisting
+console.log(a);                         // undefined
+var a = 10;
+
+console.log(b);                         // ReferenceError: b is not defined
+let b = 10;
 
 
 // temporal dead zone
-console.log(a);
-var a = 10;
+var a = 0;
+
+if (true) {
+    a = 10;                             // ReferenceError: a is not defined
+    let a;
+}
+
+
+// no redeclaring
+if (true) {
+    let a;
+    let a;                              // SyntaxError: Identifier 'a' has already been declared
+}
+
+// The `const` declaration creates a read-only reference to a value
+const number = 1;
+number = 3;                             // TypeError: Assignment to constant variable.
+
+const bar;                              // SyntaxError: Missing initializer in const declaration
+bar = 1;
+
+
+// It does not mean the value it holds is immutable, just that the variable identifier cannot be reassigned.
+const person = {};
+person.name = 'tc9011';
+
+console.log(person.name);               // tc9011
