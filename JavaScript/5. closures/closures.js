c263a23b4eee5a0774f9326182374b60a50611e8
tc9011 <tc9011@126.com>
2018-09-20 17:10:48 +0800

    feat(js): add closures

--- JavaScript/5. closures/closures.js
+++ JavaScript/5. closures/closures.js
@@ -62,3 +62,28 @@ for (var i = 1; i < 5; i++) {
     }, 1000);
   })(i);
 }
+
+
+
+function coolModule() {
+  var something = "cool";
+  var another = [1, 2, 3];
+
+  function doSomething() {
+    console.log(something);
+  }
+
+  function doAnother() {
+    console.log(another.join(' ! '));
+  }
+
+  return {
+    doSomething: doSomething,
+    doAnother: doAnother,
+  };
+}
+
+var foo = coolModule();
+
+foo.doSomething();                              // cool
+foo.doAnother();                                // 1 ! 2 ! 3
