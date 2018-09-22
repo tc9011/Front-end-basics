4fb980de36d6dd2f07761889a10ebc4715a723e1
tc9011 <tc9011@126.com>
2018-09-22 15:56:00 +0800

    feat(js): add shallow copy and deep copy

--- JavaScript/9. shallow copy and deep copy/shallow copy and deep copy.js
+++ JavaScript/9. shallow copy and deep copy/shallow copy and deep copy.js
@@ -48,3 +48,39 @@ console.log(b);                       // { name: 'a' }
 /**
  * deep copy
  */
+
+// a deep copy by recursion
+function deepCopy(source) {
+  if (!source || typeof source !== 'object') {
+    throw new Error('error arguments');
+  }
+  var targetObj = source.constructor === Array ? [] : {};
+  for (var keys in source) {
+    if (source.hasOwnProperty(keys)) {
+      if (source[keys] && typeof source[keys] === 'object') {
+        targetObj[keys] = source[keys].constructor === Array ? [] : {};
+        targetObj[keys] = deepCopy(source[keys]);
+      } else {
+        targetObj[keys] = source[keys];
+      }
+    }
+  }
+  return targetObj;
+}
+
+var o1 = {
+  arr: [1, 2, 3],
+  obj: {
+    key: 'value'
+  },
+  fun: function(){
+    return 1;
+  }
+};
+var o2 = deepCopy(o1);
+console.log(o2 === o1);                     // false
+console.log(o2.obj === o1.obj);             // false
+console.log(o2.fun === o1.fun);             // true
+
+
+// a deep copy by `JSON.stringify()` and `JSON.parse()`
