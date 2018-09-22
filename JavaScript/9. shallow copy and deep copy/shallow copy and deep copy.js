684c116a6bfe318e2501f7aea23d5517e44c6269
tc9011 <tc9011@126.com>
2018-09-22 13:19:27 +0800

    feat(js): add shallow copy and deep copy

--- JavaScript/9. shallow copy and deep copy/shallow copy and deep copy.js
+++ JavaScript/9. shallow copy and deep copy/shallow copy and deep copy.js
@@ -2,7 +2,7 @@
  * shallow copy
  */
 
-// object
+// shallow copy in the object
 var person1 = {
   name: 'tc'
 };
@@ -12,7 +12,7 @@ console.log(person2.name);          // tc
 person2.name = 'tc9011';
 console.log(person1.name);          // tc9011
 
-// array
+// shallow copy in the array
 var array1 = [0, 1, 2, 3, { name: 'a' }];
 var array2 = array1.slice();
 console.log(array2);                // [ 0, 1, 2, 3, { name: 'a' } ]
@@ -23,7 +23,26 @@ console.log(array1);                // [ 0, 1, 2, 3, { name: 'b' } ]
 console.log(array2);                // [ 0, 1, 2, 3, { name: 'b' }, 4 ]
 
 
+// common shallow copy
+function shallowCopy(source) {
+  if (!source || typeof source !== 'object') {
+    throw new Error('error arguments');
+  }
+  var targetObj = source.constructor === Array ? [] : {};
+  for (var keys in source) {
+    if (source.hasOwnProperty(keys)) {
+      targetObj[keys] = source[keys];
+    }
+  }
+  return targetObj;
+}
 
+var a = {
+  name: 'a',
+};
+
+var b = shallowCopy(a);
+console.log(b);                       // { name: 'a' }
 
 
 /**
