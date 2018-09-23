dec05695de974195eb2bfe99cb2a8574e8c10b0e
tc9011 <tc9011@126.com>
2018-09-23 20:46:54 +0800

    feat(js): add create object

--- JavaScript/3. create object/create object.js
+++ JavaScript/3. create object/create object.js
@@ -45,6 +45,48 @@ Human.prototype.fullName = function () {
 };
 
 var p1 = new Human();
-console.log(p1.firstName);                    // t
-console.log(p1.lastName);                     // c
-p1.fullName();                                // t c
+console.log(p1.firstName);                     // t
+console.log(p1.lastName);                      // c
+p1.fullName();                                 // t c
+
+
+
+// Factory Pattern
+function createPerson(name, age, job){
+  var o = new Object();
+  o.name = name;
+  o.age = age;
+  o.job = job;
+  o.sayName = function (){
+    console.log(this.name);
+  };
+  return o;
+}
+var person1 = createPerson('tc',27,'SE');
+var person2 = createPerson('zj',23,'Designer');
+
+console.log(person1.name);                     // tc
+console.log(person2.name);                     // zj
+
+
+
+// combined pattern
+function Person(name, age, job) {
+  this.name = name;
+  this.age = age;
+  this.job = job;
+  this.friends = ['tc','zj'];
+}
+Person.prototype = {
+  constructor: Person,
+  sayName: function () {
+    console.log(this.name);
+  }
+};
+var person1 = new Person('tc',29,'se');
+var person2 = new Person('zj',24,'art');
+person1.friends.push('wcx');
+console.log(person1.friends);		               //["tc", "zj", "wcx"]
+console.log(person2.friends);		               //["tc", "zj"]
+console.log(person1.friends === person2.friends);  //false
+console.log(person1.sayName === person2.sayName);  //true
