4d09f971be57e9fec8f08fe294dd3437ad792aca
tc9011 <tc9011@126.com>
2018-09-25 18:34:33 +0800

    feat(js): add Set and Map

--- JavaScript/19. Set and Map/Set and Map.js
+++ JavaScript/19. Set and Map/Set and Map.js
@@ -1,3 +1,12 @@
 var a = [1, 1, 2, 2];
 var b = [...new Set(a)];
 console.log(b);                                     // [ 1, 2 ]
+
+
+s.add(1).add(2).add(2);                             // 注意2被加入了两次
+s.size;                                             // 2
+s.has(1);                                           // true
+s.has(2);                                           // true
+s.has(3);                                           // false
+s.delete(2);
+s.has(2);                                           // false
