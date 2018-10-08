c1d4d65691c4d4700bc3aff02fc62a7e1fc907ef
tc9011 <tc9011@126.com>
2018-10-08 22:48:02 +0800

    feat(js): add debounce and throttle

---
+++ JavaScript/20. debounce and throttle/throttle.js
@@ -0,0 +1,11 @@
+function throttle(fn, interval = 300) {
+  let canRun = true;
+  return function () {
+    if (!canRun) return;
+    canRun = false;
+    setTimeout(() => {
+      fn.apply(this, arguments);
+      canRun = true;
+    }, interval);
+  };
+}
