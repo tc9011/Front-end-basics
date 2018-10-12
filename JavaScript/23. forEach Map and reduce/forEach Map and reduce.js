2855f84be40802f7c29e23ac785ff1b2d78d21fc
tc9011 <tc9011@126.com>
2018-10-12 10:35:30 +0800

    feat(js): add for each Map and reduce

--- JavaScript/23. forEach Map and reduce/forEach Map and reduce.js
+++ JavaScript/23. forEach Map and reduce/forEach Map and reduce.js
@@ -8,4 +8,11 @@ array.forEach((value, index) => {
 // NO.3 value is 3
 // NO.4 value is 4
 
+var array2 = array.map(value => value * 2);
+console.log(array2);                                        // [ 2, 4, 6, 8 ]
+array2.push(5);
+console.log(array);                                         // [ 1, 2, 3, 4 ]
+console.log(array2);                                        // [ 2, 4, 6, 8, 5 ]
 
+var array3 = array.reduce((previousValue, currentValue) => previousValue + currentValue);
+console.log(array3);                                        // 10
