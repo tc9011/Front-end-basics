78a31ba549e854c21d26b9996db850b9c3d33ae0
10227461 <tang.cheng11@zte.com.cn>
2018-10-12 10:32:59 +0800

    feat(js): add for each Map and reduce

---
+++ JavaScript/23. forEach Map and reduce/forEach Map and reduce.js
@@ -0,0 +1,11 @@
+var array = [1, 2 , 3 , 4];
+
+array.forEach((value, index) => {
+  console.log(`NO.${index + 1} value is ${value}`);
+});
+// NO.1 value is 1
+// NO.2 value is 2
+// NO.3 value is 3
+// NO.4 value is 4
+
+
