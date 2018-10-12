d331e200bb498d76f44c7fd5e6679da3cf1fd625
tc9011 <tc9011@126.com>
2018-10-12 15:15:07 +0800

    feat(js): add tail call optimization

--- JavaScript/24. tail call optimization/tail call optimization.js
+++ JavaScript/24. tail call optimization/tail call optimization.js
@@ -19,3 +19,27 @@ function f1() {
 function f2() {
   return g(x) + 1;
 }
+
+
+
+// recursive without tail call
+function recursive(x) {
+  if (x <= 0) {
+    return 1;
+  } else {
+    return x * tailRecursive(x - 1);
+  }
+}
+
+// tail recursive
+function factor(n) {
+  return recursive(n, 1);
+}
+
+function recursive(x, acc) {
+  if (x <= 0) {
+    return acc;
+  } else {
+    return recursive(x - 1, x * acc);
+  }
+}
